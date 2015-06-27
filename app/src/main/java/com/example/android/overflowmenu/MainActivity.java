57d801ff0265c8f2636f1efefcf7e0a4a28688ed
Yo-Yo <yofu1234@gmail.com>
2015-06-26 23:24:55 -0700

    Initial commit

---
+++ app/src/main/java/com/example/android/overflowmenu/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.android.overflowmenu;
+
+import android.graphics.Color;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.RelativeLayout;
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {  //this method automatically calls item from the menu when u select it from themenu
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        RelativeLayout main_view = (RelativeLayout) findViewById(R.id.main_view);
+
+        switch(item.getItemId()) {   //if user selects RED in menu, background turns red, if user selects green, background turns green, etc
+            case R.id.menu_red:
+                if(item.isChecked())
+                    item.setChecked(false);
+                else
+                    item.setChecked(true);
+                main_view.setBackgroundColor(Color.RED);
+                return true;
+            case R.id.menu_green:
+                if(item.isChecked())
+                    item.setChecked(false);
+                else
+                    item.setChecked(true);
+                main_view.setBackgroundColor(Color.GREEN);
+                return true;
+            case R.id.menu_yellow:
+                if(item.isChecked())
+                    item.setChecked(false);
+                else
+                    item.setChecked(true);
+                main_view.setBackgroundColor(Color.YELLOW);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item); //this is essentailly saying an option was selected and is not broken
+        }
+
+    }
+}
